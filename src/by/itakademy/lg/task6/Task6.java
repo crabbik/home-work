18da846189135bee434fd901a448a8e850350d4b
olegsergeev <olehrubchinskiy@gmail.com>
2017-10-05 20:12:05 +0300

    no message

---
+++ src/by/itakademy/lg/task6/Task6.java
@@ -0,0 +1,16 @@
+package by.itakademy.lg.task6;
+
+public class Task6 {
+
+	public static void main(String[] args) {
+
+		int day = 18;
+		int month = 5;
+		int year = 1991;
+
+		System.out.println(String.format("Следующий день %s-%s-%s", day + 1,
+				month, year));
+
+	}
+
+}
