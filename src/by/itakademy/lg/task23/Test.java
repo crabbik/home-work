583f4bf1126ba5760c74605217cf7db90a852279
User-ПК\User <crab_@mail.ru>
2017-10-14 14:32:42 +0300

    no message

--- src/by/itakademy/lg/task23/Test.java
+++ src/by/itakademy/lg/task23/Test.java
@@ -2,19 +2,20 @@ package by.itakademy.lg.task23;
 
 import java.util.ArrayList;
 
-
 public class Test {
 
 	public static void main(String[] args) {
 		ArrayList<Slot> slots = new ArrayList<>();
-		slots.add(new Slot(100, 30));
-		slots.add(new Slot(50, 60));
-		slots.add(new Slot(10, 50));
+		slots.add(new Slot(100, 10));
+		slots.add(new Slot(50, 1));
+		slots.add(new Slot(10, 1));
 
 		ATM atm = new ATM(slots);
 
-		atm.cacheOut(520);
-
+		atm.cacheOut(900);
+		atm.cacheOut(100);
+		atm.cacheOut(50);
+		atm.cacheOut(50);
 	}
 
 }
